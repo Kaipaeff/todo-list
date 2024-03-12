40cb34f29c1871462dde13294a3ae1231c8353c3
Aslanbek <kaipaeff@gmail.com>
2024-03-12 20:59:01 +0300

    linear progressbar added into Navbar

--- src/components/Navbar/Navbar.styles.ts
+++ src/components/Navbar/Navbar.styles.ts
@@ -2,6 +2,9 @@ import { styled } from 'styled-components';
 import { borderColor, textColor } from '../../styles/Colors';
 
 export const NavbarStyled = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   width: 100%;
   padding: 12px 32px;
   margin-bottom: 24px;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
+import { Box, Typography } from '@mui/material';
+
 import { NavbarStyled } from './Navbar.styles';
 
 function Navbar() {
+  const [progress, setProgress] = useState(67);
+
+  function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
+    return (
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box sx={{mr: 1}}>
+          <Typography variant="body2" color="text.secondary">{`Completed:`}</Typography>
+        </Box>
+        <Box sx={{ width: '100%', mr: 2 }}>
+          <LinearProgress variant="determinate" {...props} />
+        </Box>
+        <Box sx={{ minWidth: 35 }}>
+          <Typography variant="body2" color="text.secondary" >{`${Math.round(props.value)}%`}</Typography>
+        </Box>
+      </Box>
+    );
+  }
+
   return (
-    <>
-      <NavbarStyled>All Tasks | Active Tasks | Completed Tasks</NavbarStyled>
-    </>
+    <NavbarStyled>
+      All Tasks | Active Tasks | Completed Tasks
+      <Box sx={{ width: '30%' }}>
+        <LinearProgressWithLabel value={progress} color="success" />
+      </Box>
+    </NavbarStyled>
   );
 }
 
