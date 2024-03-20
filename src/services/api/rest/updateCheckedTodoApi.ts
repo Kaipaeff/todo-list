fa2abc117d62476537efe58c40fc61ade976e6a0
Aslanbek <kaipaeff@gmail.com>
2024-03-20 22:18:01 +0300

    Toggled API server

--- src/services/api/rest/updateCheckedTodoApi.ts
+++ src/services/api/rest/updateCheckedTodoApi.ts
@@ -1,5 +1,5 @@
-// const url = 'https://cb17b4fb6a0456c5.mokky.dev/todos';
-const url = 'http://localhost:3000/todos'; //reserve locale JSON-Server
+const url = 'https://cb17b4fb6a0456c5.mokky.dev/todos';
+// const url = 'http://localhost:3000/todos'; //reserve locale JSON-Server
 
 export const updateCheckedTodoApi = async (id: number, data: { completed: boolean }) => {
   try {
